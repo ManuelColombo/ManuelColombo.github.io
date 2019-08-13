1fb3feb558b0ddda7fa2802c9b85cf68125b767d
Manuel Colombo <manuel.colombo@idean.com>
2019-08-13 23:38:15 +0200

    Inserito js per la vista scroll a pagina singola

--- script/manuelcolombo.js
+++ script/manuelcolombo.js
@@ -1,39 +1,33 @@
-apiKey = "yM898o598jOYl77EYKtVy8OnJbOjzRGl";
+/* my personal Js*/
+function ready(callback) {
+  // in case the document is already rendered
+  if (document.readyState != 'loading') callback();
+  // modern browsers
+  else if (document.addEventListener)
+    document.addEventListener('DOMContentLoaded', callback);
+  // IE <= 8
+  else
+    document.attachEvent('onreadystatechange', function() {
+      if (document.readyState == 'complete') callback();
+    });
+}
 
-/**
- * Get JSONP data
- * @param  {String}   url      The JSON URL
- * @param  {Function} callback The function to run after JSONP data loaded
- */
-var getJSONP = function ( url, callback ) {
+ready(function() {
+  // do something
 
-    // Create script with url and callback (if specified)
-    var ref = window.document.getElementsByTagName( 'script' )[ 0 ];
-    var script = window.document.createElement( 'script' );
-    script.src = url + (url.indexOf( '?' ) + 1 ? '&' : '?') + 'callback=' + callback;
-
-    // Insert script tag into the DOM (append to <head>)
-    ref.parentNode.insertBefore( script, ref );
-
-    // After the script is loaded (and executed), remove it
-    script.onload = function () {
-        this.remove();
-    };
-
-};
-
-// Callback function
-var UserInfo = function ( data ) {
-    console.log( data );
-};
-
-// Callback function
-var Project = function ( data ) {
-    console.log( data );
-};
-
-// Get JSON
-getJSONP( "https://api.behance.net/v2/users/bananascivolare?client_id=" + apiKey + "&callback=myCallbackFunction", 'UserInfo' );
-getJSONP( "http://www.behance.net/v2/projects/48484463?client_id=" + apiKey + "&callback=myCallbackFunction", 'Project' );
-
-
+  onePageScroll('.main', {
+    sectionContainer: 'section', // sectionContainer accepts any kind of selector in case you don't want to use section
+    easing: 'ease', // Easing options accepts the CSS3 easing animation such "ease", "linear", "ease-in",
+    // "ease-out", "ease-in-out", or even cubic bezier value such as "cubic-bezier(0.175, 0.885, 0.420, 1.310)"
+    animationTime: 1000, // AnimationTime let you define how long each section takes to animate
+    pagination: true, // You can either show or hide the pagination. Toggle true for show, false for hide.
+    updateURL: false, // Toggle this true if you want the URL to be updated automatically when the user scroll to each page.
+    beforeMove: function(index) {}, // This option accepts a callback function. The function will be called before the page moves.
+    afterMove: function(index) {}, // This option accepts a callback function. The function will be called after the page moves.
+    loop: false, // You can have the page loop back to the top/bottom when the user navigates at up/down on the first/last page.
+    keyboard: true, // You can activate the keyboard controls
+    responsiveFallback: false, // You can fallback to normal page scroll by defining the width of the browser in which
+    // you want the responsive fallback to be triggered. For example, set this to 600 and whenever
+    // the browser's width is less than 600, the fallback will kick in.
+  });
+});
